f412711bd1faa33db1f516a5922f660d6f592c36
Ramzi Bouzaiene <ramzi.bouzaiene@sastec-group.com>
2023-03-30 23:26:51 +0100

    setup project layouts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,58 @@
 import React from "react";
-
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  Homepage,
+  Exchanges,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from "./components";
+import "./App.css";
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />}></Route>
+              <Route exact path="/exchanges" element={<Exchanges />}></Route>
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies />}
+              ></Route>
+              <Route
+                exact
+                path="/crypto/:coinId"
+                element={<CryptoDetails />}
+              ></Route>
+              <Route exact path="/news" element={<News />}></Route>
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Cryptoverse <br />
+            All Rights Reserved Ramzi
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/CryptoDetails.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CryptoDetails = () => {
+  return (
+    <div>
+      <h1>Crypto Details</h1>
+    </div>
+  );
+};
+
+export default CryptoDetails;
