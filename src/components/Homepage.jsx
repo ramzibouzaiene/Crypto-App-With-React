7a4e63bda1d0428c27c4c2f18b09e3bafb94a796
Ramzi Bouzaiene <ramzi.bouzaiene@sastec-group.com>
2023-03-30 23:49:31 +0100

    setup the home page

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import { Typography, Row, Col, Statistic } from "antd";
+import { Link } from "react-router-dom";
+
+const { Title } = Typography;
 
 const Homepage = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <Title level={2} className="heading">
+        Global Crypto Stats
+      </Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title="Total Cryptocurrencies" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Exchanges" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Market Cap" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total 24h Volume" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Markets" value="5" />
+        </Col>
+      </Row>
+    </>
   );
 };
 
